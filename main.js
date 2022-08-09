4579c880cb5cc055f8e2f7bee400b2317ac50a09
Miyara <83151604+Miyara-m@users.noreply.github.com>
2022-08-08 18:00:43 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var game_start = false;
+var sound = "";
+function preload()
+{
+
+}
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+}
+function draw()
+{
+    
+}
+function next()
+{
+    sound++;
+}
+function start()
+{
+    game_start =  true;
+}
